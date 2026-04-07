bfa4ff2f3fb8c1a28d3b4093c67bab139e02ca9d
sneekydart <callumjosemacdonald@gmail.com>
2026-04-07 21:35:56 +1000

    third

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+def Dispense(Number, Amount, Rate):
+    print(Number, Amount, Rate)
+
 # Open the file in read mode
 with open("example.txt", "r") as file:
     # Loop through each line in the file
     for line in file:
         # Print the line (strip removes extra newline characters)
         print(line.strip())
+    
+Dispense(10, 15, 20)
